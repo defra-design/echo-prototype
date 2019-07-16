fd3191feae6ec34a973dfbf4a261506b319e5511
David Olsan <davidolsan@davidolsan.com>
2019-07-16 11:43:26 +0100

    make it better1

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Apply for an Export Health Certificate (EHC)',
+  serviceName: 'Service name goes here',
 
   // Default port that prototype runs on
   port: '3000',
--- app/routes.js
+++ app/routes.js
@@ -1,30 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-// Version 1.0-0 routing
-require('./routes/1-0-0/dashboard.js')(router);
-require('./routes/1-0-0/ehcUpload.js')(router);
-require('./routes/1-0-0/manualUpload.js')(router);
-require('./routes/1-0-0/exa.js')(router);
-require('./routes/1-0-0/checkAndSubmit.js')(router);
+// Add your routes here - above the module.exports line
 
-// Version 1.0-1 routing
-require('./routes/1-0-1/dashboard.js')(router);
-require('./routes/1-0-1/ehcUpload.js')(router);
-require('./routes/1-0-1/manualUpload.js')(router);
-require('./routes/1-0-1/exa.js')(router);
-require('./routes/1-0-1/checkAndSubmit.js')(router);
-
-// Version 2849 routing
-require('./routes/2849/dashboard.js')(router);
-require('./routes/2849/ehcUpload.js')(router);
-require('./routes/2849/manualUpload.js')(router);
-require('./routes/2849/supportingDocumentsUpload.js')(router);
-require('./routes/2849/exa.js')(router);
-require('./routes/2849/checkAndSubmit.js')(router);
-
-router.get('/', function (req, res) {
-  res.render('home');
-});
-
-module.exports = router;
+module.exports = router
