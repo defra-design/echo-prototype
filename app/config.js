3adf5c75aed7398a3cd174fe0c362acc4b4ce6df
David Olsan <davidolsan@davidolsan.com>
2019-08-06 11:23:10 +0100

    e2e exporter 1

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Exporter service',
+  serviceName: '',
 
   // Default port that prototype runs on
   port: '3000',
