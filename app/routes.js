27b64df3d84c3ff23fc92de40dd4b40faf364158
Richard <richard.hatton@digital.education.gov.uk>
2019-10-16 10:26:01 +0100

    adding in 3rd version of the skip quesions, this is breaking it down to yes no answers

--- app/routes.js
+++ app/routes.js
@@ -4,4 +4,5 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 require('./routes/ehc-7006-ab-version4/routes.js')(router);
 require('./routes/ehc-7006-ab-version4-1/routes.js')(router);
+require('./routes/ehc-7006-ab-version4-3/routes.js')(router);
 module.exports = router
