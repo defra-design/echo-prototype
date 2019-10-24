44ac3e5b69e064b821c8d8278ce24035cacbc40d
Richard <richard.hatton@digital.education.gov.uk>
2019-10-24 13:27:13 +0100

    making product page more dynamic

--- app/routes/ehc-8327/routes.js
+++ app/routes/ehc-8327/routes.js
@@ -14,9 +14,6 @@ module.exports = function(router) {
   const certificate= "8327EHC"
 
 
-
-
-
 function isDupucate(arr,name){
   for (var i = 0; i < arr.length; i++) {
 
@@ -28,14 +25,21 @@ function isDupucate(arr,name){
 }
 
 function findPage(arr,id){
-  for (var i = 0; i < arr.length; i++) {
+  console.log("----")
+  console.log("finding page based on ID")
+  console.log("id = " + id)
 
+  for (var i = 0; i < arr.length; i++) {
+    console.log("checking = " + i)
+    console.log("page id = " + arr[i].page)
+    console.log("page title = " + arr[i].title)
     if (arr[i].page == id) {
       return arr[i];
     }
   }
   return false;
 }
+
 
 router.get('/'+base_url+'certificate/check-your-*', function(req, res) {
   req.session.data.certificate = certificate
@@ -57,16 +61,21 @@ router.post('/'+base_url+'certificate/exa/certifier-confirm-address', function(r
 
 
 router.post('/'+base_url+'certificate/page', function(req, res, next) {
+  var query = ""
+  if(req.query.next == "product-list"){
+    query+=("id="+req.query.id)
+  }
   req.session.data.completed[req.query.id] = req.query.id
-  res.redirect(301, '/' + base_url + 'certificate/'+req.query.next);
+  res.redirect(301, '/' + base_url + 'certificate/'+req.query.next+"?"+query);
 })
 
 
 router.get('/'+base_url+'certificate/page', function(req, res) {
     var id =  parseInt(req.query.id) || 0;
+
   res.render(base_url+'certificate/page', {
     "query": req.query,
-    "page":findPage(req.session.data[database].pages,id)
+    "page":findPage(req.session.data[database].pages, id)
   }, function(err, html) {
     if (err) {
       if (err.message.indexOf('template not found') !== -1) {
@@ -82,11 +91,29 @@ router.get('/'+base_url+'certificate/page', function(req, res) {
 router.get('/'+base_url+'certificate/exa/your-commodity', function(req, res) {
   res.render(base_url+'certificate/exa/your-commodity', {
     "query": req.query,
-    "commodities":req.session.data.ehc8327.commodities
+    "commodities":req.session.data[database].commodities
   }, function(err, html) {
     if (err) {
       if (err.message.indexOf('template not found') !== -1) {
         return res.render(file_url + '/exa/your-commodity');
+      }
+      throw err;
+    }
+    res.send(html);
+  })
+})
+router.get('/'+base_url+'certificate/product-list', function(req, res) {
+  var id = req.query.id || 2;
+
+  console.log( findPage(req.session.data[database].pages,id))
+  var product = findPage(req.session.data[database].pages,id)
+  res.render(base_url+'certificate/product-list', {
+    "query": req.query,
+    "product":product
+  }, function(err, html) {
+    if (err) {
+      if (err.message.indexOf('template not found') !== -1) {
+        return res.render(file_url + '/certificate/product-list');
       }
       throw err;
     }
@@ -128,14 +155,11 @@ router.post('/'+base_url+'certificate/supporting-documents', function(req, res) 
     req.session.data.uploaded_files.push({"name":file,"description":description,"ID":id})
   }
   res.redirect(301, '/' + base_url + 'certificate/supporting-documents'+query);
-
 })
 
   // this adds query to all pages and will be called if no other get routing exists.
   router.get('/' + base_url + '*', function(req, res) {
     console.log("default get routing page for: "+base_url + req.params[0])
-
-
     res.render(base_url + req.params[0], {
       "query":req.query
     });
