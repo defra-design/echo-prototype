c8b670e34ced0ed23b2cf7e8a9ae87a8c2b5ccda
Richard <richard.hatton@defra.gov.uk>
2020-06-18 15:01:24 +0100

    form builder : admin can set task list title

--- app/routes/v7/routes.js
+++ app/routes/v7/routes.js
@@ -4,6 +4,9 @@ module.exports = function(router) {
   var nunjucks = require('nunjucks');
 var env = new nunjucks.Environment();
 
+require('./EXP-8528-task-list-grouping.js')(router)
+
+
 env.addFilter('shorten', function(str, count) {
     return str.slice(0, count || 5);
 });
