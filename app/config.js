bb8a6c5bbb9fb2350a42081aae3ed475887a0eba
Chris Harding <chris@transparentux.co.uk>
2022-05-27 12:52:57 +0100

    Created a new version of the form builder section of the prototype (v1-5) for the WIP migrate certificates to new certifier organisation work.

--- app/config.js
+++ app/config.js
@@ -7,7 +7,7 @@ module.exports = {
   serviceName: '',
 
   // Default port that prototype runs on
-  port: '3000',
+  port: '4000',
 
   // Enable or disable password protection on production
   useAuth: 'true',
